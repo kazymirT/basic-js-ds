25ffcb2960fb4fb52bb8572cb26dd4c7f4bceb4e
Taras <kazemir.t23.23@gmail.com>
2023-04-15 11:42:53 +0300

    feat: add solution task queue

--- src/queue.js
+++ src/queue.js
@@ -14,16 +14,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  constructor() {
+    this.list = [];
+    this.listElement;
+  }
 
   getUnderlyingList() {
+    return this.list[0];
   }
 
   enqueue(value) {
+    if(this.list.length===0){
+    this.listElement={value:value, next: null};
+    this.list.push(this.listElement);
+    }else{
+    let current=this.list[0];
+    while(current.next){
+        current=current.next;
+    }
+    current.next={value:value, next:null};
+    }
   }
 
   dequeue() {
-  }
+    let firstElement=this.list[0].value;
+    if(this.list[0].next===null){
+    this.list[0].value=null;
+    this.list[0].next=null;
+    }else{
+    this.list[0].value=this.list[0].next.value;
+    this.list[0].next=this.list[0].next.next;
+    }
+  return firstElement;
 }
+ }
 
 module.exports = {
   Queue
